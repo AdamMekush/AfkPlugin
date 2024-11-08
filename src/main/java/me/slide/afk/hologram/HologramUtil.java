bb507808c4b2dec62fa43672fd904f1dd78b2323
Adam Mekush <60749996+AdamMekush@users.noreply.github.com>
2024-11-08 17:21:08 +0100

    Added Exception to HologramUtil

--- src/main/java/me/slide/afk/hologram/HologramUtil.java
+++ src/main/java/me/slide/afk/hologram/HologramUtil.java
@@ -9,10 +9,14 @@ import java.util.List;
 import java.util.UUID;
 
 public class HologramUtil {
-    public static void createHologram(UUID uuid){
-        List<String> list = new ArrayList<>();
-        list.add("AFK");
-        Hologram hologram = DHAPI.createHologram(uuid.toString(), Bukkit.getPlayer(uuid).getLocation().add(0, 2.3, 0), list);
-        hologram.showAll();
+    public static void createHologram(UUID uuid) throws IllegalArgumentException {
+        try {
+            List<String> list = new ArrayList<>();
+            list.add("AFK");
+            Hologram hologram = DHAPI.createHologram(uuid.toString(), Bukkit.getPlayer(uuid).getLocation().add(0, 2.3, 0), list);
+            hologram.showAll();
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e);
+        }
     }
 }
